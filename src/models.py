3db41653fd1aae0f8a14443514ae3ca46755f8ed
MBello21 <magarbello@gmail.com>
2026-02-01 18:47:47 +0000

    Creacion API recetas

--- src/models.py
+++ src/models.py
@@ -1,19 +1,89 @@
+from typing import List
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Column, Table, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+recetas_fav = Table(
+    "recetas_fav",
+    db.metadata,
+    Column("id", db.Integer, primary_key=True),
+    Column("usuario_id", ForeignKey("user.id"), nullable=False),
+    Column("receta_id", ForeignKey("recetas.id"), nullable=False)
+)
+
+ingredientes_receta = Table(
+    "ingredientes_recetas",
+    db.metadata,
+    Column("id", db.Integer, primary_key=True),
+    Column("receta_id", ForeignKey("recetas.id"), nullable=False),
+    Column("ingredientes_id", ForeignKey("ingredientes.id"), nullable=False)
+)
+
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(unique=False, nullable=False)
+    lastname: Mapped[str] = mapped_column(unique=False, nullable=False)
+    favorito: Mapped[List["Recetas"]] = relationship(
+        secondary=recetas_fav,
+        back_populates="favoritos_de"
+    )
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "favorito": [f.name for f in self.favorito]
+        }
+
+
+class Recetas(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=False, nullable=False)
+    ingredientes: Mapped[List["Ingredientes"]] = relationship(
+        secondary=ingredientes_receta,
+        back_populates="ingrediente_de"
+    )
+    favoritos_de: Mapped[List["User"]] = relationship(
+        secondary=recetas_fav,
+        back_populates="favorito"
+    )
+
+    def __repr__(self):
+        return f"{self.name}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "ingredientes": [i.serialize() for i in self.ingredientes]
+        }
+
+
+class Ingredientes(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=False, nullable=False)
+    tipo: Mapped[str] = mapped_column(nullable=False)
+    ingrediente_de: Mapped[List["Recetas"]] = relationship(
+        secondary=ingredientes_receta,
+        back_populates="ingredientes"
+    )
+
+    def __repr__(self):
+        return f"{self.name}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "tipo": self.tipo
         }
